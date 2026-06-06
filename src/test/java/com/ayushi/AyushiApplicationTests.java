ea6585dc5eb7348d3b164fbd870d0c1a77080162
amanahmed0404-dotcom <amanahmed0404@gmail.com>
2026-06-06 21:26:41 +0530

    Update AmanApplicationTests.java

--- src/test/java/com/ayushi/AyushiApplicationTests.java
+++ src/test/java/com/ayushi/AyushiApplicationTests.java
@@ -1,10 +1,10 @@
-package com.ayushi;
+package com.aman;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class AyushiApplicationTests {
+class AmanAhmedApplicationTests {
 
 	@Test
 	void contextLoads() {
